8775689aaadc861a84c72cad83bcaac176fed5c2
manan1113 <manan8562.beaift25@chitkara.edu.in>
2025-08-20 23:10:43 +0530

    Add files via upload

---
+++ lecture1.py
@@ -0,0 +1,43 @@
+# sum of data types (int,float,long,char)
+a = 10
+b = 20 
+c = a+b
+print(c)
+
+a = 11.23
+b = 5.67
+c = a+b
+print(c)
+
+a = 98462697
+b = 67547365
+c = a+b
+print(c)
+
+a = "y"
+b = "o"
+c = a+b
+print(c)
+
+# sum of data types (int,float,long,char) using input
+a = int(input("enter no.:"))
+b = int(input("enter no.:"))
+c = a+b
+print(c)
+
+a = float(input("enter no.:"))
+b = float(input("enter no.:"))
+c = a+b
+print(c)
+
+# variables
+name = "ram" 
+age = 23
+print("my name is :",name)
+print("my age is :",age)
+
+# using inputs in varibales
+name = input("enter name :")
+age = input("enter age :")
+print("welcome", name)
+print("age = ", age)

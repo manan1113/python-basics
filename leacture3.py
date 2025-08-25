222182bdae6bc2a488ecd674d5339f58123ddeef
manan1113 <manan8562.beaift25@chitkara.edu.in>
2025-08-25 22:04:28 +0530

    Add files via upload

---
+++ leacture3.py
@@ -0,0 +1,42 @@
+# loops in python 
+# for i in range(1,1000):
+#     print("numbers :",i)
+
+# fruits=["cherry","mango","grapes"]
+# for fruit in fruits:
+#     print("i like :",fruit)
+
+# # while condition
+# # code block
+# i=1
+# while 1<=5:
+#     print("number :",i)
+#     i+=1
+
+# # print the first 10 natural numbers 
+# for i in range(1,11):
+#     print(i)
+
+# # print multiplication table of a number 5 
+# i = 1
+# while i<=10:
+#     print(5*i)
+#     i+=1
+
+# print all even no. from 1 to 100 
+# for i in range(1,101):
+#     if(i%2==0):
+#      print(i)
+
+# print the reverse of numbers fron n to 1 
+# i = 100
+# while i>=1:
+#     print(i)
+#     i-=1
+
+# find factorial of a number using for loop 
+n = 5
+fact = 1
+for i in range(1,n+1):
+    fact*=i
+print("factorial =",fact)

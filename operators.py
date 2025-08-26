b5938063318b92f26f71d58ffebc3d1fece18ade
manan1113 <manan8562.beaift25@chitkara.edu.in>
2025-08-26 21:22:55 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,63 @@
+# # Arithmatic operators
+a = 50
+b = 20
+c = a + b
+print(c)
+
+a = 10
+b = 20
+c = a - b
+print(c)
+
+a = 10
+b = 20
+c = a * b
+print(c)
+
+a = 10
+b = 20
+c = a / b
+print(c)
+
+a = 10
+b = 20
+c = a // b
+print(c)
+
+a = 10
+b = 20
+c = a % b
+print(c)
+
+# # Assignment operators 
+a = 20
+print(a)
+
+a = 100
+a += 10
+print(a)
+
+a = 40
+a -= 20
+print(a)
+
+a = 50
+a *= 10
+print(a)
+
+a = 40
+a /= 20
+print(a)
+
+# # logical operators 
+a = int(input("enter no. :"))
+b = int(input("enter no. :"))
+print(a<b or a==b)
+
+a = int(input("enter no. :"))
+b = int(input("enter no. :"))
+print(a>b and a==b)
+
+a = int(input("enter no. :"))
+b = int(input("enter no. :"))
+print(not(a>b))

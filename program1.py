ef42425cc08f09116ba404994d700bfa90439afe
manan1113 <manan8562.beaift25@chitkara.edu.in>
2025-08-26 21:23:24 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,20 @@
+# write a program to find the sum,difference,product and division of two numbers entered by the user 
+a = int(input("Enter first no.:"))
+b = int(input("Enter second no.:"))
+sum = a+b
+print("sum is :",sum)
+
+a = int(input("Enter first no.:"))
+b = int(input("Enter second no.:"))
+diff = a-b
+print("difference is :",diff)
+
+a = int(input("Enter first no.:"))
+b = int(input("Enter second no.:"))
+product = a*b
+print("product is :",product)
+
+a = int(input("Enter first no.:"))
+b = int(input("Enter second no.:"))
+div = a/b
+print("division is :",div)

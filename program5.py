13efdb95282605ce3adc80c2122ea36336310e0b
manan1113 <manan8562.beaift25@chitkara.edu.in>
2025-08-26 21:25:05 +0530

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,7 @@
+# write a program to find the remainder and quotient when one no. is divided by the another 
+a = int(input("enter first no. :"))
+b = int(input("enter second no.:"))
+quotient = a//b
+remainder = a % b
+print("quotient = ",quotient)
+print("remainder = ",remainder)

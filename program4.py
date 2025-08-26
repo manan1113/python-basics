9146de964fbf330c86d7c25519d06c136a27a00b
manan1113 <manan8562.beaift25@chitkara.edu.in>
2025-08-26 21:24:39 +0530

    Add files via upload

---
+++ program4.py
@@ -0,0 +1,6 @@
+# check whether a number is greater than 100 and divisible by 5 
+num = int(input("Enter no.:"))
+if(num > 100 and num % 5 == 0):
+    print("number is gretaer than 100 and divisible by 5")
+else:
+    print("no")
